04089d7b9c85f2c772cb7912833e54f3bfab617e
Cruifer <38588039+Cruifer@users.noreply.github.com>
2020-12-10 17:19:03 +0100

    that was kinda stupid

--- advent02/advent2.cpp
+++ advent02/advent2.cpp
@@ -3,28 +3,34 @@
 
 using namespace std;
 
+int digits(int n){
+    int k = 1;
+    while(n >= 10){
+        n /= 10;
+        k++;
+    }
+    return k;
+}
+
 int main(){
     const int N = 1000;
-    int n = 0, l = 0, firstn[N], secondn[N];
-    string line, lett[N], pass[N];
+    int n = 0, firstn, secondn;
+    string line, lett, pass;
     ifstream myfile("advent2.txt");
     if(myfile.is_open()){
         while(getline(myfile, line)) {
-            firstn[l] = stoi(line);
-            line.erase(0, to_string(firstn[l]).length() + 1);
-            secondn[l] = stoi(line);
-            line.erase(0, to_string(secondn[l]).length() + 1);
-            lett[l] = line.substr(0, 1);
-            line.erase(0, lett[l].length() + 2);
-            pass[l] = line;
-            l++;
+            firstn = stoi(line);
+            line.erase(0, digits(firstn) + 1);
+            secondn = stoi(line);
+            line.erase(0, digits(secondn) + 1);
+            lett = line.substr(0, 1);
+            line.erase(0, 3);
+            pass = line;
+            if(pass.substr(firstn - 1, 1) == lett xor pass.substr(secondn - 1, 1) == lett){
+                n++;
+            }
         }
         myfile.close();
-    }
-    for(int i = 0; i < N; i++){
-        if(pass[i].substr(firstn[i] - 1, 1) == lett[i] xor pass[i].substr(secondn[i] - 1, 1) == lett[i]){
-            n++;
-        }
     }
     cout << n;
     return 0;
