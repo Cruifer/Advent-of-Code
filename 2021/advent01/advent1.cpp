eb2347c214fb73ec041bcbe9abaeb09e54b8ed7f
Cruifer <38588039+Cruifer@users.noreply.github.com>
2021-12-01 22:17:44 +0100

    2k21

---
+++ 2021/advent01/advent1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    vector<int> vect;
+    int k = 0, p = 0;
+    string line;
+    ifstream adv1("advent1.txt");
+    if(adv1.is_open()){
+        while(getline(adv1, line)){
+            vect.push_back(stoi(line));
+        }
+        adv1.close();
+    }
+    for(int i = 0; i < vect.size() - 1; ++i){
+       if(vect[i + 1] > vect[i]){
+            ++k;
+       }
+    }
+    cout << "part 1: " << k << endl;
+    for(int j = 0; j < vect.size() - 3; ++j){
+        if(vect[j + 3] > vect[j]){
+            ++p;
+        }
+    }
+    cout << "part 2: " << p << endl;
+    return 0;
+}
